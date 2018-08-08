4d4c9217be574571596b70b1cfbd95b9d4cf0e19
Raul Montero <raul.amontero@gmail.com>
2018-08-08 18:17:36 -0400

    heroku push

--- src/Components/Bills.js
+++ src/Components/Bills.js
@@ -7,6 +7,7 @@ class Bills extends Component {
       <div className="Bills">
         <p>This section is under construction and will arrive soon 🏗 ...</p>
         <p>Expect to see a list of bills brought to the floors of Congress and the Senate, with individal pages for more detailed information per each bill.</p>
+        <p>2018</p>
       </div>
     );
   }
